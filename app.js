ff60cd3a39ccdeb994feac4b0bec602991039278
Arjun Raj <arjunrajvilakkat@outlook.com>
2025-10-18 15:06:15 +0530

    two endpoints for creating and fetching posts, set up CORS

--- app.js
+++ app.js
@@ -4,6 +4,19 @@ const feedRoutes = require("./routes/feed");
 
 const app = express();
 
+app.use(express.json());
+
+// Setting up CORS
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE, OPTIONS"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 app.use("/feed", feedRoutes);
 
 app.listen(8080);
